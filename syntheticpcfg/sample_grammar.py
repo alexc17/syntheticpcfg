88fec10dc60d1d9e3597263c969fe5093763ad13
alexc <alexsclark@gmail.com>
2018-08-24 10:51:03 +0100

    Added sample grammar module.

---
+++ syntheticpcfg/sample_grammar.py
@@ -0,0 +1,12 @@
+#sample_grammar.py
+
+import argparse
+
+
+parser = argparse.ArgumentParser(description='Create a randomly generated PCFG')
+
+parser.add_argument("outputfilename", description="File where the resulting PCFG will be stored")
+
+parser.add_argument("--terminals", description="Number of terminals", def)
+
+
