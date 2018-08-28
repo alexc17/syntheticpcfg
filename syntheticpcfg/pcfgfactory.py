817578a6c9cc41fcb658bbd505e65297deb547f2
alexc <alexsclark@gmail.com>
2018-08-28 16:55:53 +0100

    added some command line options but not tested yet.

--- syntheticpcfg/pcfgfactory.py
+++ syntheticpcfg/pcfgfactory.py
@@ -3,6 +3,8 @@ from collections import Counter
 from collections import defaultdict
 import numpy.random
 import numpy as np
+import scipy.misc
+
 import inside
 import cfg
 import cfgfactory
@@ -15,7 +17,7 @@ LENGTH_EM_MAX_LENGTH = 20
 class LengthDistribution:
 
 	def __init__(self):
-		self.weights = [0.0, 10.0, 9.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0]
+		self.weights = [0.0, 9.0, 10.0, 8.0, 7.0, 6.0, 5.0, 4.0, 3.0]
 
 
 	def cds(self):
@@ -26,11 +28,17 @@ class LengthDistribution:
 			4720,  3049, 2225,  1664, 1217,  819,  414,  185,  84,  54,  32 ]
 		
 
-	def ztnb(self):
+ 
+
+	def ztnb(self,r=5,p=0.5,max_length=20):
 		"""
 		Weights are sampled from a zero truncated negative binomial.
 		"""
-		pass
+		probs = [0.0] 
+		for k in range(1,max_length+1):
+			probability = scipy.misc.comb(k + r -1,k) * (p ** k) * ((1.0  -p) ** r)
+			probs.append(probability)
+		self.weights = probs
 
 	def poisson(self, alpha):
 		"""
@@ -69,11 +77,13 @@ import pitmanyor
 
 class LexicalPitmanYor:
 
-	def __init__(self,d=0.5,alpha=1.0):
+	def __init__(self,d=0.9,alpha=1.0):
 		"""
 		d is discount
-		alpha is strength
-		d is zero gives something close to 
+		alpha is concentration parameter: alpha close to zero means all concentrated in a single one, 
+		alpha greater than 1 means more spread out.
+		d is zero gives something close to Dirichlet 
+		d = 0.95 or so gives you something close to 
 		"""
 		self.d = d
 		self.alpha = alpha
@@ -92,9 +102,8 @@ class PCFGFactory:
 	def __init__(self):
 
 		self.cfgfactory = cfgfactory.CFGFactory()
-		self.length_weights = LengthDistribution().weights
+		self.length_distribution = LengthDistribution()
 		self.lexical_distribution = LexicalDirichlet()
-		self.current = None
 
 	def train_unary_once(self, my_pcfg, a, max_length):
 		posteriors = defaultdict(float)
@@ -103,7 +112,7 @@ class PCFGFactory:
 		insidec = inside.InsideComputation(my_pcfg)
 		for l in range(1 , max_length+1):
 			s = tuple([ a for i in range(l)])
-			w = self.length_weights[l]
+			w = self.length_distribution.weights[l]
 			if w > 0:
 				insidec.add_posteriors(s, posteriors, w)
 			#print("Posteriors",l,posteriors)
--- syntheticpcfg/sample_grammar.py
+++ syntheticpcfg/sample_grammar.py
@@ -17,12 +17,16 @@ parser.add_argument("--lexicalproductions", help="Number of lexical productions"
 parser.add_argument("--seed",help="Choose random seed",type=int)
 
 parser.add_argument("--pitmanyor",help="Use Pitman Yor process for lexical probs",action="store_true")
+parser.add_argument("--dirichletparam",help="Set Dirichlet for lexical probs",type=int,default=1.0)
+
+parser.add_argument("--cdslength", help="Use distribution of lengths from corpus of English child directed speech.", action=store_true)
+parser.add_argument("--corpuslength", help="Use distribution of lengths from a chosen corpus. Filename of corpus.")
 
 args = parser.parse_args()
 
 nts = args.nonterminals
 
-if args.binaryproductions >  nts * nts * nts:
+if args.binaryproductions >  0.5 * nts * nts * nts:
 	raise ValueError("Impossibly high number of binary rules.")
 
 
@@ -38,9 +42,13 @@ factory.cfgfactory.lexical_rules = args.lexicalproductions
 
 if args.pitmanyor:
 	factory.lexical_distribution = pcfgfactory.LexicalPitmanYor()
+else:
+	factory.lexical_distribution = pcfgfactory.LexicalDirichlet(dirichlet=args.dirichlet)
 
-
-
+if args.cdslength:
+	factory.length_distribution.cds()
+elif args.corpuslength:
+	factory.length_distribution.from_corpus(args.corpuslength)
 
 # sample and save
 pcfg = factory.sample()
